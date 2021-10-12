fd2d8e17d8355aa5a8676810288c255c596904c5
Vishva Patel <88160041+vishva7049@users.noreply.github.com>
2021-10-12 10:52:06 -0400

    used range function

---
+++ bulidtower.py
@@ -0,0 +1,8 @@
+def tower_builder(n_floors):
+    result = []
+    width = (n_floors * 2) - 1
+    for x in range(1, 2* n_floors, 2):
+        stars = x * '*'
+        line = stars.center(width)
+        result.append(line)
+    return result
