2c2462c084041e6499f50098a5cd8a7b1fa7e8f0
Vishva Patel <88160041+vishva7049@users.noreply.github.com>
2021-11-09 12:53:35 -0500

    Add files via upload

---
+++ order_filter.py
@@ -0,0 +1,24 @@
+def order_food(lst): 
+    #your code here
+    # s1 = dict(lst)
+
+    dic1 = {} 
+    
+    for dict in lst:
+        if(dict['meal'] in dic1):
+            dic1[dict['meal']] += 1
+        else:
+            dic1[dict['meal']] = 1
+    return dic1
+
+
+list1 = [
+    { 'firstName': 'Noah', 'lastName': 'M.', 'country': 'Switzerland', 'continent': 'Europe', 'age': 19, 'language': 'C', 'meal': 'vegetarian' },
+    { 'firstName': 'Anna', 'lastName': 'R.', 'country': 'Liechtenstein', 'continent': 'Europe', 'age': 52, 'language': 'JavaScript', 'meal': 'standard' },
+    { 'firstName': 'Ramona', 'lastName': 'R.', 'country': 'Paraguay', 'continent': 'Americas', 'age': 29, 'language': 'Ruby', 'meal': 'vegan' },
+    { 'firstName': 'George', 'lastName': 'B.', 'country': 'England', 'continent': 'Europe', 'age': 81, 'language': 'C', 'meal': 'vegetarian' },
+]
+
+
+print(order_food(list1))
+
